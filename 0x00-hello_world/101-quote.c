b31c04889ea7bc5712bbf0dcc41a78670682a31e
Marwane <bouj2265@gmail.com>
2023-06-15 08:58:34 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main()
   {
-  const char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+  const char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
   write(STDOUT_FILENO, x, strlen(x));
   return 0;
   }
