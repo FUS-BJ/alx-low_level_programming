929624a5fb041b78a87c740f45fcca54af1462aa
Marwane <bouj2265@gmail.com>
2023-06-15 09:02:50 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  * main -output and that piece of art is useful\" - Dora Korpar, 2015-10-19
  * Return: Always 0 (Success)
  */
-int main()
-  {
-  const char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(STDOUT_FILENO, x, strlen(x));
-  return (1);
-  }
+int main(void)
+ {
+ const char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ write(STDOUT_FILENO, x, strlen(x));
+ return (1);
+ }
