7f47939380be8be2b00785233feb94afb585c5ea
Marwane <bouj2265@gmail.com>
2023-06-15 05:08:46 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,12 @@
  */
 int main(void)
 {       
-char str1[52];
-strcpy(str1, "\"Programming is like building a multilingual puzzle");
-puts (str1); /*out put str1*/       
-return(0);
+
+	char str1[52];
+
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+	puts(str1); /*out put str1*/       
+
+	return (0);
 }
