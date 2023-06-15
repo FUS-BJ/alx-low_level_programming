ef122c29e72ed115b4887612b95dba135e27d0a4
Marwane <bouj2265@gmail.com>
2023-06-15 04:58:47 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include <string.h>
-
+/*output 
+ * \"Programming is like building a multilingual puzzle
+ * not using printf
+ */
 int main (void)
 {       
-	char str1[52];
-	
-	strcpy(str1,"\"Programming is like building a multilingual puzzle");
-	
-	puts(str1);
-       
-	return(0);
+char str1[52];
+strcpy (str1, "\"Programming is like building a multilingual puzzle");
+puts (str1);       
+return (0);
 }
