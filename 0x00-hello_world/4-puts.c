76fe9bf4c6acedc59a627bd1066b0cfd5fa35d17
Marwane <bouj2265@gmail.com>
2023-06-15 05:21:14 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-/*output \"Programming is like building a multilingual puzzle
+/**output \"Programming is like building a multilingual puzzle
  * not using printf*/
 int main(void)
 {
