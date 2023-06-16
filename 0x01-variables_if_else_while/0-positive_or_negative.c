deb08573eddb6bef00e7e37dec3907af8a61fc04
Marwane <bouj2265@gmail.com>
2023-06-16 04:12:10 -0700

    m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf(is positive)
+		printf("is positive")
 	}
 	else if (n = 0)
 	{
-		printf(is zero)
+		printf("is zero")
 	}
 	else 
 	{
-		pirintf(is negative)
+		pirintf("is negative")
 	}		
 
 	return (0);
