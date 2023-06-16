52e8fe866ddcb53e6e3ae29b7eaa22b9afb8acf5
Marwane <bouj2265@gmail.com>
2023-06-16 16:15:24 -0700

    m

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - is for printing the alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+       	int F;
+
+for (F = 'z'; F >= 'a'; F--)
+{
+	putchar(F);
+}
+putchar('\n');
+return (0);
+}
