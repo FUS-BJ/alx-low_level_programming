b2b18f480d2027dde898e97887e660767462637d
Marwane <bouj2265@gmail.com>
2023-06-16 05:52:10 -0700

    -m-

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include<stdio.h>
 /*
- * main - for comparing if number is positif
+ * main - is for comparing if a number is positif
  * Return: Always 0 (Success)
  */
 int main(void)
