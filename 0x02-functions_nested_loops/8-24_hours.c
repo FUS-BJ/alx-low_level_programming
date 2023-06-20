42db1fd072bd7353633577cad2044e135ffe53f3
Marwane <bouj2265@gmail.com>
2023-06-20 14:03:39 -0700

    m

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,26 +9,21 @@ void jack_bauer(void)
 {
 	char a;
 	char b;
-	char c;
-	char d;
 
-	for (a = '0'; a <= '2'; a++)
+
+	for (a = 0; a <= 23; a++)
 {
-	for (b = '0'; a <= '9'; b++)
+	for (b = '0'; b <= 59; b++)
 	{
-	for (c = '0'; c <= '6'; c++)
-	{
-	for (d = '0'; c <= '9'; d++)
-	{
-		_putchar(d);
-	}
-	_putchar(c);
+	_putchar((a / 10) + '0');
+
+	_putchar((a % 10) + '0');
 	_putchar(':');
-	}
-	_putchar(b);
-	}
-	_putchar(a);
-	if (a == '2' && b == '4')
-		break;
+
+	_putchar((b / 10) + '0');
+
+	_putchar((b % 10) + '0');
+	_putchar('\n');	
+}
 }
 }
