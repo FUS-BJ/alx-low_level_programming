60fd95d39e370c1028251bf415aa44c80f61141f
Marwane <bouj2265@gmail.com>
2023-06-26 16:39:10 -0700

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print a string in revers
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int rev = 0;
+	int z;
+	
+	while (*s != '\0')
+	{
+		rev++;
+		s++;
+	}
+	s--
+	for (z = rev; z > 0; z--)
+	{
+		_puthcar(*s);
+		s--
+	}
+	_putchar('\n');
