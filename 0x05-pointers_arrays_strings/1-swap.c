eee26e4887da5072704a97d4941da18055894744
Marwane <bouj2265@gmail.com>
2023-06-26 14:04:11 -0700

    m

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap betwen var
+ * @*a: variable number 1
+ * @*b: variable niumber 2
+ */
+void swap_int(int a, int b)
+{
+	int G = *a;
+	*a = *b;
+	*b = G;
+}
