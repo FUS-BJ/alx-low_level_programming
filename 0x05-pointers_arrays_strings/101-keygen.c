e919e59915be591ea3109fcb2d6bc9ffcc27d4ea
Marwane <bouj2265@gmail.com>
2023-06-26 18:17:33 -0700

    m

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - genetrate random numbers
+ * Return: always 0
+ */
+int main(void)
+{
+	int N;
+	srand(time(0));
+	N = rand();
+	printf("%i\n",N);
+	Return (0);
+}
